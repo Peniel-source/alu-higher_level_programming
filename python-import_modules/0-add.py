d8ae1aa4c517186704a1055ae2b30b474929d1f7
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-06-27 14:12:54 +0200

    import_modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,8 @@
 a = 1
 b = 2
 
-from add_0 import add
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,14 +2,16 @@
 a = 10
 b = 5
 
-from calculator_1 import add, sub, mul, div
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-output = "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}".format(
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    output = "{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}".format(
         a, b, result_add, a, b, result_sub, a, b, result_mul, a, b, result_div
     )
 
-print(output)
+    print(output)
