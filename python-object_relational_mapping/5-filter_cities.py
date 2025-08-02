2282daf8ca3f3342c56a002309e1691a7cb882a5
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-08-02 23:57:01 +0200

    mm

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Lists all cities from the database"""
+"""
+Program prints out all the cities of a state
+"""
 
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    xx = cursor.fetchall()
-    for i in xx:
-        print(i)
-    cursor.close()
-    db.close()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
+    states = cursor.fetchall()
+
+    print(", ".join([state[1] for state in states]))
