0366b2557b6677073e45d6658f9fef4d8fee19df
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-08-02 22:27:07 +0200

    aa

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+"""Fetch a url"""
 
 import requests
-from requests import Session
+
 
 if __name__ == "__main__":
-    session = Session()
-    requ = session.get('https://intranet.hbtn.io/status')
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(requ.text))
-    print("\t- content:", requ.text)
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
